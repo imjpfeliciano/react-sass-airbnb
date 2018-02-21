2458343d1b34ac0a65a22f47924a8bfc16118850
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-02-21 16:11:11 -0600

    fix space after row

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ const App = () => {
     <div className='app-header'>
       <h1>My first React Project!</h1>
       <p>React-Sass-Airbnb Boilerplate to start working on</p>
-      <div className='row logos'>
-        <div className='col-sm-2'><img src='./assets/webpack-logo.png' alt='webpack' /></div>
-        <div className='col-sm-2'><img src='./assets/react-logo.png' alt='react' /></div>
-        <div className='col-sm-2'><img src='./assets/babel-logo.svg' alt='babel' /></div>
-        <div className='col-sm-2'><img src='./assets/sass-logo.png' alt='sass' /></div>
-        <div className='col-sm-2'><img src='./assets/eslint-logo.svg' alt='eslint' /></div>
-        <div className='col-sm-2'><img src='./assets/airbnb-logo.svg' alt='airbnb' /></div>
+      <div className='logos'>
+        <div className='row'>
+          <div className='col-sm-2'><img src='./assets/webpack-logo.png' alt='webpack' /></div>
+          <div className='col-sm-2'><img src='./assets/react-logo.png' alt='react' /></div>
+          <div className='col-sm-2'><img src='./assets/babel-logo.svg' alt='babel' /></div>
+          <div className='col-sm-2'><img src='./assets/sass-logo.png' alt='sass' /></div>
+          <div className='col-sm-2'><img src='./assets/eslint-logo.svg' alt='eslint' /></div>
+          <div className='col-sm-2'><img src='./assets/airbnb-logo.svg' alt='airbnb' /></div>
+        </div>
       </div>
     </div>
   );
