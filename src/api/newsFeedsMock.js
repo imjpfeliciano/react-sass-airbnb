3329f3212acdbd9c2769cf466b78c6b2d8f47dbe
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-03-22 10:50:44 -0600

    add mocked news feeds

---
+++ src/api/newsFeedsMock.js
@@ -0,0 +1,39 @@
+const news = [
+  {
+    src: 'https://zartnerds.files.wordpress.com/2015/10/thumbnail.png',
+    title: 'Feed #1',
+    date: '12-Mar-18',
+    description: 'Ut veniam ad minim cillum magna minim in laboris aliquip et sit. Pariatur fugiat magna nisi tempor. Nulla ullamco esse laboris nostrud cillum ut. Eiusmod ullamco ea laborum proident ullamco do labore occaecat duis consequat dolore do cupidatat aute. Veniam esse mollit consectetur incididunt enim proident Lorem. Ad voluptate ea qui est Lorem ullamco. Irure ut commodo reprehenderit Lorem officia.',
+    url: '#',
+  },
+  {
+    src: 'https://zartnerds.files.wordpress.com/2015/10/thumbnail.png',
+    title: 'Feed #2',
+    date: '12-Mar-18',
+    description: 'Eiusmod reprehenderit sint amet Lorem amet in. Non proident ea ullamco nisi dolore nisi nulla ut in. Nulla fugiat non ullamco adipisicing. Id nisi dolor excepteur eiusmod minim tempor velit consequat magna anim id magna. Commodo minim sint nulla aliqua veniam aliqua.',
+    url: '#',
+  },
+  {
+    src: 'https://zartnerds.files.wordpress.com/2015/10/thumbnail.png',
+    title: 'Feed #3',
+    date: '12-Mar-18',
+    description: 'Amet velit occaecat ut aliquip. Sunt excepteur dolor et ea voluptate ipsum magna elit duis aliqua. Amet cillum laborum eiusmod esse amet labore eu exercitation.',
+    url: '#',
+  },
+  {
+    src: 'https://zartnerds.files.wordpress.com/2015/10/thumbnail.png',
+    title: 'Feed #4',
+    date: '12-Mar-18',
+    description: 'Sunt deserunt exercitation in laborum dolore ut aliquip fugiat et eu consequat minim laborum. Tempor eu et irure do sunt est consequat ea. Laboris velit commodo ut Lorem. Est irure id velit sint aliquip sit nulla aliquip enim sit anim. Occaecat laboris pariatur sit incididunt duis deserunt cupidatat.',
+    url: '#',
+  },
+  {
+    src: 'https://zartnerds.files.wordpress.com/2015/10/thumbnail.png',
+    title: 'Feed #5',
+    date: '12-Mar-18',
+    description: 'Enim sunt sint aute minim commodo reprehenderit sint. Velit ullamco incididunt exercitation deserunt Lorem. Est exercitation adipisicing anim Lorem irure commodo aliquip Lorem sint dolore minim.',
+    url: '#',
+  },
+];
+
+export default news;
